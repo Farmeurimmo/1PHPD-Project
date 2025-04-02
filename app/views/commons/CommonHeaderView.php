8e3f1e1779b37e49d02bceb0330c1f9853cc327f
PetitFlora <flora.petit@supinfo.com>
2025-04-02 12:31:40 +0200

    Added menu

--- app/views/commons/CommonHeaderView.php
+++ app/views/commons/CommonHeaderView.php
@@ -1,8 +1,22 @@
-<header>
-    <nav>
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/">...</a></li>
+<nav>
+    <div id="navigation">
+        <ul id="navigation">
+            <li><a href="#">Home</a></li>
+            <li>
+                <span>Category</span>
+                <ul class="category">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Comedy</a></li>
+                </ul>
+            </li>
         </ul>
-    </nav>
-</header>
+    </div>
+    <!-- If user not conected then id=notconected display flex -->
+    <div class="users" id="notconected" style="display: flex;">
+        <span>Sign In/Sign Up</span>
+    </div>
+    <div class="users" id="conected" style="display: none;">
+        <span>Cart</span>
+        <span>Profil</span>
+    </div>
+</nav>
