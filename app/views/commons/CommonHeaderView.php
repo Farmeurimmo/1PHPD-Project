90a1b634e31abe3ea2cb2f445ce051ee68308d19
PetitFlora <flora.petit@supinfo.com>
2025-04-13 10:28:23 +0200

    update: user drop-down + titles category pages

--- app/views/commons/CommonHeaderView.php
+++ app/views/commons/CommonHeaderView.php
@@ -12,35 +12,33 @@
         </ul>
     </div>
     <?php if (isset($_SESSION["username"])) : ?>
-        <div class="users" id="conected" style="display: flex; gap: 30px;">
-            <a href="/1PHPD/my/cart"><i class="fa-solid fa-cart-shopping"></i>
-                <?php if (isset($_COOKIE["cart"])) : ?>
-                    <?php
-                    echo count(json_decode($_COOKIE["cart"], true));
-                    ?>
-                <?php else : ?>
-                    <?php echo 0; ?>
-                <?php endif; ?>
-            </a>
-
-            <a href="/1PHPD/my/profile">
-                <span><i class="fa-solid fa-user"></i></span>
-                <span><?php echo $_SESSION["username"]; ?></span>
-            </a>
+        <div class="users" style="display: flex;">
+            <div class="cartdiv">
+                <a href="/1PHPD/my/cart"><i class="fa-solid fa-cart-shopping"></i>
+                    <?php if (isset($_COOKIE["cart"])) : ?>
+                        <?php
+                        echo count(json_decode($_COOKIE["cart"], true));
+                        ?>
+                    <?php else : ?>
+                        <?php echo 0; ?>
+                    <?php endif; ?>
+                </a>
+            </div>
+            
             <div class="dropdown">
                 <button class="dropbtn">
                     <span><i class="fa-solid fa-user"></i></span>
                     <span><?php echo $_SESSION["username"]; ?></span>
                 </button>
                 <div class="dropdown-content">
-                    <a href="#">My Profil</a>
-                    <a href="#">My Films</a>
-                    <a href="#">Sign Out</a>
+                    <a href="/1PHPD/my/profile">My Profil</a>
+                    <a href="/1PHPD/my/films">My Films</a>
+                    <a href="/1PHPD/auth/logout">Sign Out</a>
                 </div>
             </div>
         </div>
     <?php else : ?>
-        <div class="users" id="notconected" style="display: flex;">
+        <div class="users" style="display: flex;">
             <span><a href="/1PHPD/auth/">Sign In/Sign Up</a></span>
         </div>
     <?php endif; ?>
